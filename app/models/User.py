b91f1481983b97eba1cd87d004bb26354aadceb9
jeongseonwoong <jeongseonwoong@LAPTOP-JQUTIV0S>
2024-10-02 20:29:54 +0900

    parameter name update

--- app/models/User.py
+++ app/models/User.py
@@ -2,13 +2,19 @@ import logging
 import re
 
 class User:
-    def __init__(self,id,username,role,created_at):
+    def __init__(self,id,name,memberId,password,role,avartar,phoneNumber,studentNumber,birth,email):
         self._validate_std_num(id)
 
         self.id = id
-        self.username = username
+        self.name = name
+        self.memberId = memberId
+        self.password = password
         self.role = role
-        self.created_at = created_at
+        self.avartar = avartar
+        self.phoneNumber = phoneNumber
+        self.studentNumber = studentNumber
+        self.birth = birth
+        self.email = email
 
     def _validate_std_num(self, student_num):
         logging.basicConfig(level=logging.INFO)
@@ -19,3 +25,4 @@ class User:
             log.info("/validId 컨트롤러로 들어온 Id가 regex를 만족하지 않음.")
             raise ValueError("/validId 컨트롤러로 들어온 Id가 regex를 만족하지 않음.")
 
+    def _validate_pw(self,)
