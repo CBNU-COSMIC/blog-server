88892e07b626032e1d0bc8fa049c314c26fd6087
jeongseonwoong <jeongseonwoong@LAPTOP-JQUTIV0S>
2024-10-02 20:15:12 +0900

    feat: validate id -> validate student num으로 이름 변경

--- app/models/User.py
+++ app/models/User.py
@@ -3,14 +3,14 @@ import re
 
 class User:
     def __init__(self,id,username,role,created_at):
-        self._validate_id(id)
+        self._validate_std_num(id)
 
         self.id = id
         self.username = username
         self.role = role
         self.created_at = created_at
 
-    def _validate_id(self, id):
+    def _validate_std_num(self, id):
         logging.basicConfig(level=logging.INFO)
         validPattern = r'^\d{6,11}$'
         log = logging.getLogger("myapp")
@@ -19,5 +19,3 @@ class User:
             log.info("/validId 컨트롤러로 들어온 Id가 regex를 만족하지 않음.")
             raise ValueError("/validId 컨트롤러로 들어온 Id가 regex를 만족하지 않음.")
 
-    def _validate_pw(self, password):
-        logging.basicConfig(level=logging.INFO)
