03c621d849aa3e030f37ea43d0437ee808e6e559
jeongseonwoong <jeongseonwoong@LAPTOP-JQUTIV0S>
2024-10-02 20:38:54 +0900

    브랜치 이동을 위한 커밋

--- app/models/User.py
+++ app/models/User.py
@@ -19,10 +19,9 @@ class User:
     def _validate_std_num(self, student_num):
         logging.basicConfig(level=logging.INFO)
         validPattern = r'^\d{6,11}$'
-        log = logging.getLogger("myapp")
+        log = logging.getLogger("user_validate")
         log.info("/validId 컨트롤러로 들어온 Id=%s", student_num)
         if re.match(validPattern, str(student_num)) is None:
             log.info("/validId 컨트롤러로 들어온 Id가 regex를 만족하지 않음.")
             raise ValueError("/validId 컨트롤러로 들어온 Id가 regex를 만족하지 않음.")
 
-    def _validate_pw(self,)
