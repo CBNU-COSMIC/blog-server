bc266494907428dc22c37b882f27827f5653b229
jeongseonwoong <jeongseonwoong@LAPTOP-JQUTIV0S>
2024-10-02 20:27:57 +0900

    parameter name update

--- app/models/User.py
+++ app/models/User.py
@@ -10,12 +10,12 @@ class User:
         self.role = role
         self.created_at = created_at
 
-    def _validate_std_num(self, id):
+    def _validate_std_num(self, student_num):
         logging.basicConfig(level=logging.INFO)
         validPattern = r'^\d{6,11}$'
         log = logging.getLogger("myapp")
-        log.info("/validId 컨트롤러로 들어온 Id=%s", id)
-        if re.match(validPattern, str(self.id)) is None:
+        log.info("/validId 컨트롤러로 들어온 Id=%s", student_num)
+        if re.match(validPattern, str(student_num)) is None:
             log.info("/validId 컨트롤러로 들어온 Id가 regex를 만족하지 않음.")
             raise ValueError("/validId 컨트롤러로 들어온 Id가 regex를 만족하지 않음.")
 
