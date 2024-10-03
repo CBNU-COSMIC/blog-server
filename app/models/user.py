d34a5c39e3e704d2f84260858702216dd7691889
MinSeok Oh <minseok.dev@gmail.com>
2024-10-03 16:38:08 +0900

    refactor: User __init__ 리팩토링
    - 타입 힌트를 명시합니다
    - 비공개 인스턴스 변수명에 __를 추가합니다
    - 메서드 사용 시 인자 이름을 명시합니다
    - 비공개 인스턴스 변수의 getter를 생성합니다
    - regex를 클래스 변수로 변경합니다

--- app/models/user.py
+++ app/models/user.py
@@ -1,29 +1,64 @@
-import logging
+from datetime import datetime
 import re
 
 
 class User:
-    def __init__(self,id,name,member_id,password,role,avartar,phone_number,student_number,birth,email):
-        self.log=log
-        self._validate_student_num(student_number)
+    student_number_regex = re.compile(r"^\d{6,11}$")
 
-        self.id = id
-        self.name = name
-        self.member_id = member_id
-        self.password = password
-        self.role = role
-        self.avartar = avartar
-        self.phone_number = phone_number
-        self.student_number = student_number
-        self.birth = birth
-        self.email = email
+    def __init__(self, id: str, name: str, member_id: str, password: str, role: str, avatar: str, phone_number: str,
+                 student_number: str, birth: datetime, email: str):
+        self._validate_student_number(student_number=student_number)
+
+        self.__id = id
+        self.__name = name
+        self.__member_id = member_id
+        self.__password = password
+        self.__role = role
+        self.__avatar = avatar
+        self.__phone_number = phone_number
+        self.__student_number = student_number
+        self.__birth = birth
+        self.__email = email
+
+    @property
+    def id(self) -> str:
+        return self.__id
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def member_id(self) -> str:
+        return self.__member_id
+
+    @property
+    def password(self) -> str:
+        return self.__password
+
+    @property
+    def role(self) -> str:
+        return self.__role
 
         #re.match: 문자열과 패턴을 입력하면 그때마다 입력된 문자열과 패턴이 일치하는지 비교
         #re.compile: 패턴을 객체형태로 저장해 놓고 문자열만 넣어주면 패턴에 맞는지 비교 ->여러번 사용 시 성능 최적화하기 좋음.
+    @property
+    def avatar(self) -> str:
+        return self.__avatar
 
-    def _validate_student_num(self, student_num):
-        self.log.info("student_number=%s", student_num)
-        if re.match("^\d{6,11}$",student_num) is None:
-            self.log.info("student_number이 유효하지 않음.")
-            raise ValueError("student_number이 regex를 만족하지 않음.")
+    @property
+    def phone_number(self) -> str:
+        return self.__phone_number
+
+    @property
+    def student_number(self) -> str:
+        return self.__student_number
+
+    @property
+    def birth(self) -> datetime:
+        return self.__birth
+
+    @property
+    def email(self) -> str:
+        return self.__email
 
