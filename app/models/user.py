9209572245a76b825ead3af7bfed4aff1cb29afa
Sin Sohi <sohi8451@naver.com>
2024-10-05 20:23:35 +0900

    feat: 아이디 유효성 확인

--- app/models/user.py
+++ app/models/user.py
@@ -4,10 +4,12 @@ import re
 
 class User:
     student_number_regex = re.compile(r"^\d{6,11}$")
+    member_id_regex = re.compile(r"^[a-z0-9]+$")
 
     def __init__(self, id: str, name: str, member_id: str, password: str, role: str, avatar: str, phone_number: str,
                  student_number: str, birth: datetime, email: str):
         self._validate_student_number(student_number=student_number)
+        self._validate_student_member_id(member_id=member_id)
 
         self.__id = id
         self.__name = name
@@ -69,3 +71,13 @@ class User:
 
         if User.student_number_regex.match(student_number) is None:
             raise ValueError(f"현재 학번: {student_number}")
+        
+    def _validate_member_id(self, member_id: str) -> None:
+        """
+        아이디의 유효성을 검사합니다.
+        """
+        if member_id is None:
+            raise ValueError(f"현재 아이디: None")
+        
+        if User.member_id_regex.match(member_id) is None:
+            raise ValueError(f"현재 아이디: {member_id}")
