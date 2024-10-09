d30a2001bb957c07b1743e194a45b32d7e7dc791
Hyunjiiing <107828202+Hyunjiiing@users.noreply.github.com>
2024-10-09 19:14:28 +0900

    글의 제목은 유효한 문자열이어야 한다 (#115)
    * fix: 인스턴스 변수 private으로 변경
    
    - "__"를 앞에 붙여 private으로 변경함
    - User의 의존성을 제거함
    - getter를 모두 정의함
    
    * feat: 글 제목 유효성 검사 구현
    
    - None과 빈 문자열을 검사함
    - "<>"은 대체 문자로 변경함
    
    * test: 글 제목 유효성 검사 테스트 작성

--- app/models/post.py
+++ app/models/post.py
@@ -1,18 +1,60 @@
-from app.models.user import User
 from datetime import datetime
 
 
 class Post:
-    def __init__(self, id: str, title: str, attribute: str, content: str, writer: User, board: str,
+    def __init__(self, id: str, title: str, attribute: str, content: str, user_id: str, board: str,
                  created_at: datetime):
-        self.id = id
-        self.title = title
-        self.attribute = attribute
-        self.content = content
-        self.writer = writer
-        self.board = board
-        self.created_at = created_at
+        self._validate_title(title=title)
+
+        self.__id = id
+        self.__title = self._make_valid_title(title)
+        self.__attribute = attribute
+        self.__content = content
+        self.__user_id = user_id
+        self.__board = board
+        self.__created_at = created_at
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def attribute(self):
+        return self.__attribute
+
+    @property
+    def content(self):
+        return self.__content
+
+    @property
+    def user_id(self):
+        return self.__user_id
+
+    @property
+    def board(self):
+        return self.__board
+
+    @property
+    def created_at(self):
+        return self.__created_at
 
     def __repr__(self):
-        return (f"Post(id={self.id!r}, title={self.title!r}, attribute={self.attribute!r}, content={self.content!r},"
-                f" writer={self.writer!r}, board={self.board!r}, created_at={self.created_at!r})")
+        return (f"Post(id={self.__id!r}, title={self.__title!r}, attribute={self.__attribute!r}, content={self.__content!r},"
+                f" writer={self.__user_id!r}, board={self.__board!r}, created_at={self.__created_at!r})")
+
+    def _validate_title(self, title: str) -> None:
+        """
+        글 제목의 유효성을 검사합니다.
+        """
+        if not title:
+            raise ValueError(f"현재 제목: {title}")
+
+    def _make_valid_title(self, title: str) -> str:
+        """
+        글 제목을 유효한 제목으로 변경합니다.
+        """
+        return title.replace('<', '&lt;').replace('>', '&gt;')
--- tests/models/test_post.py
+++ tests/models/test_post.py
@@ -1,35 +1,22 @@
 from unittest import TestCase
 
 from app.models.post import Post
-from app.models.user import User
 from datetime import datetime
 
 
 class TestPost(TestCase):
     def test_valid_post_creation(self):
         # Given
-        user_id = "test"
-        name = "test"
-        member_id = "test"
-        password = "test1234!"
-        role = "test"
-        avatar = "test"
-        phone_number = "01012345678"
-        student_number = "202000000"
-        birth = datetime.now()
-        email = "test@test.com"
-        writer = User(id=user_id, name=name, member_id=member_id, password=password, role=role, avatar=avatar,
-                      phone_number=phone_number, student_number=student_number, birth=birth, email=email)
-
         post_id = "1"
         title = "title"
         attribute = "attribute"
         content = "content"
+        user_id = "1"
         board = "board"
         created_at = datetime.now()
 
         # When
-        post = Post(id=post_id, title=title, attribute=attribute, content=content, writer=writer, board=board,
+        post = Post(id=post_id, title=title, attribute=attribute, content=content, user_id=user_id, board=board,
                     created_at=created_at)
 
         # Then
@@ -37,37 +24,56 @@ class TestPost(TestCase):
         self.assertEqual(post.title, title)
         self.assertEqual(post.attribute, attribute)
         self.assertEqual(post.content, content)
-        self.assertEqual(post.writer, writer)
+        self.assertEqual(post.user_id, user_id)
         self.assertEqual(post.board, board)
         self.assertEqual(post.created_at, created_at)
 
     def test_post_repr(self):
         # Given
-        user_id = "test"
-        name = "test"
-        member_id = "test"
-        password = "test1234!"
-        role = "test"
-        avatar = "test"
-        phone_number = "01012345678"
-        student_number = "202000000"
-        birth = datetime.now()
-        email = "test@test.com"
-        writer = User(id=user_id, name=name, member_id=member_id, password=password, role=role, avatar=avatar,
-                      phone_number=phone_number, student_number=student_number, birth=birth, email=email)
-
         post_id = "1"
         title = "title"
         attribute = "attribute"
         content = "content"
+        user_id = "1"
         board = "board"
         created_at = datetime.now()
 
         # When
-        post = Post(id=post_id, title=title, attribute=attribute, content=content, writer=writer, board=board,
+        post = Post(id=post_id, title=title, attribute=attribute, content=content, user_id=user_id, board=board,
                     created_at=created_at)
 
         # Then
         self.assertEqual(repr(post),
                          f"Post(id={post_id!r}, title={title!r}, attribute={attribute!r}, content={content!r}, "
-                         f"writer={writer!r}, board={board!r}, created_at={created_at!r})")
+                         f"writer={user_id!r}, board={board!r}, created_at={created_at!r})")
+
+    def test_invalid_post_creation(self):
+        # Given
+        test_cases = [
+            # title
+            ("1", None, "test", "test", "1", "test", datetime.now()),
+            ("1", "", "test", "test", "1", "test", datetime.now())
+        ]
+
+        # Expect
+        for test_case in test_cases:
+            with self.assertRaises(ValueError):
+                Post(*test_case)
+
+    def test_post_creation_with_html_tags_in_title(self):
+        # Given
+        post_id = "1"
+        title = "<br/>"
+        attribute = "attribute"
+        content = "content"
+        user_id = "1"
+        board = "board"
+        created_at = datetime.now()
+
+        # When
+        post = Post(id=post_id, title=title, attribute=attribute, content=content, user_id=user_id, board=board,
+                    created_at=created_at)
+
+        # Then
+        self.assertNotIn("<", post.title)
+        self.assertNotIn(">", post.title)
