3cae3b62e34c80d92bb517b295ed9683536654e0
jeongseonwoong <jeongseonwoong@LAPTOP-JQUTIV0S>
2024-10-31 23:38:56 +0900

    feat/12

--- app/crud/user_crud.py
+++ app/crud/user_crud.py
@@ -1,10 +1,12 @@
 from sqlalchemy.orm import Session
 
+from app.models.user import User
 from app.models.user_entity import UserEntity
 
 
 def get_user_by_id(db: Session, id):
     return db.query(UserEntity).filter(UserEntity.id == id).first()
 
-def register(db: Session, user: UserEntity):
-    return db.add(user)
+def create_user(db: Session, user: User):
+    user_entity = UserEntity(id= user.id, name= user.name, member_id= user.member_id, password= user.password, role= user.role, avatar= user.avatar, phone_number= user.phone_number, student_number= user.student_number, birth= user.birth, email= user.email)
+    return db.add(user_entity)
