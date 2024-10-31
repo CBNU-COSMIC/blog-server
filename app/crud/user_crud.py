d26b410241a0b4bdf51d1fdb3e0ae654a5cee811
Sin Sohi <sohi8451@naver.com>
2024-10-31 18:06:30 +0900

    feat/8: 이미 가입한 회원은 가입할 수 없다

--- app/crud/user_crud.py
+++ app/crud/user_crud.py
@@ -1,7 +1,15 @@
 from sqlalchemy.orm import Session
-
 from app.models.user_entity import UserEntity
-
+from typing import Optional
 
 def get_user_by_id(db: Session, id):
     return db.query(UserEntity).filter(UserEntity.id == id).first()
+
+# 학번으로 사용자 조회
+def get_user_by_student_number(db: Session, student_number: str) -> Optional[UserEntity]:
+    return db.query(UserEntity).filter(UserEntity.student_number == student_number).first()
+
+# 학번으로 사용자 아이디 조회
+def get_member_id_by_student_number(db: Session, student_number: str) -> Optional[str]:
+    user = db.query(UserEntity).filter(UserEntity.student_number == student_number).first()
+    return user.member_id if user else None
