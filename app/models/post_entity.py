f6b7afabc41ed406e210c8adff6880112d0d83c0
jeongseonwoong <jeongseonwoong@LAPTOP-JQUTIV0S>
2024-10-31 15:10:19 +0900

    feat/121

--- app/models/post_entity.py
+++ app/models/post_entity.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 Base = declarative_base()
 
-class PostDAO(Base):
+class PostEntity(Base):
     __tablename__ = 'Post'
 
     id = Column(String, primary_key=True)
@@ -18,7 +18,3 @@ class PostDAO(Base):
     def __repr__(self):
         return f"<User(id={self.id}, title='{self.title}', attribute='{self.attribute}', content='{self.content}', user_id='{self.user_id}', board='{self.board}', created_at='{self.created_at}')>"
 
-    def __eq__(self, other):
-        if isinstance(other, PostDAO):
-            return self.id == other.id and self.title == other.title and self.attribute == other.attribute and self.content == other.content and self.user_id == other.user_id and self.board == other.board and self.created_at == other.created_at
-        return False
