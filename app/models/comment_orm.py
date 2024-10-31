084855ddc88de941d74c9c85c467b89cb7e6e1cb
jeongseonwoong <jeongseonwoong@LAPTOP-JQUTIV0S>
2024-10-31 14:50:14 +0900

    feat/120

--- app/models/comment_orm.py
+++ app/models/comment_orm.py
@@ -4,18 +4,19 @@ import datetime
 
 Base = declarative_base()
 
-class CommentORM(Base):
+class CommentDAO(Base):
     __tablename__ = 'Comment'
 
     id = Column(String, primary_key=True)
     user_id = Column(String, ForeignKey('Member.id'), nullable=False)
     content = Column(String, ForeignKey('Post.id'))
     parent_id = Column(String, ForeignKey('Comment.id'))  # 대댓글을 위한 자기 참조 외래 키
-    comment_date = Column(DateTime,)
+    comment_date = Column(DateTime)
 
-    def __init__(self, id: str, user_id: str, content: str, parent_id: str, comment_date: datetime):
-        self.id = id
-        self.user_id = user_id
-        self.content = content
-        self.parent_id = parent_id
-        self.comment_date = comment_date
+    def __repr__(self):
+        return f"<User(id={self.id}, user_id='{self.user_id}', content='{self.content}', parent_id='{self.parent_id}', comment_date='{self.comment_date}')>"
+
+    def __eq__(self, other):
+        if isinstance(other, CommentDAO):
+            return self.id == other.id and self.user_id == other.user_id and self.content == other.content and self.parent_id == other.parent_id and self.comment_date == other.comment_date
+        return False
