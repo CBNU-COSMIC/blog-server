1a5bb0411b7a4639ac22b1ea766d7854d4cca4d7
Hyunjiiing <h.jilluminate@gmail.com>
2024-11-01 05:09:57 +0900

    refactor: 테스트 코드를 위한 TODO 작성

--- app/crud/post_crud.py
+++ app/crud/post_crud.py
@@ -5,6 +5,7 @@ from app.schemas.posts_read_dto import PostsReadDTO
 
 
 def get_posts_by_board_id(db: Session, board_id: int, page: int = 1) -> list[PostsReadDTO]:
+    # TODO: 추후 User, Board이 완료되면 테스트 코드 작성
     offset = (page - 1) * 10
     posts = (
         db.query(PostCreateDTO)
