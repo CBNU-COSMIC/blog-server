7aeb7d3ba6073caeb2a4d22a37cd551c3614e223
Hyunjiiing <h.jilluminate@gmail.com>
2024-11-01 04:20:17 +0900

    feat: post 속성 제거 및 게시판 식별자 추가

---
+++ app/entity/post_entity.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, DateTime, ForeignKey, Integer
+
+from config.db_config import Base
+
+
+class PostEntity(Base):
+    __tablename__ = 'Post'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(50), nullable=False)
+    content = Column(String(1000), nullable=False)
+    member_id = Column(Integer, ForeignKey('Member.id'), nullable=False)
+    board_id = Column(Integer, ForeignKey('Board.id'), nullable=False)
+    created_at = Column(DateTime)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, title='{self.title}', attribute='{self.attribute}', content='{self.content}', user_id='{self.user_id}', board='{self.board}', created_at='{self.created_at}')>"
