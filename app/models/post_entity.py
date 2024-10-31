338c72ec1f02f76690c49d3979c065464600c7c7
jeongseonwoong <jeongseonwoong@LAPTOP-JQUTIV0S>
2024-11-01 00:37:50 +0900

    refactor: dev -> feat/9 충돌 해결

--- app/models/post_entity.py
+++ app/models/post_entity.py
@@ -8,10 +8,9 @@ class PostEntity(Base):
 
     id = Column(Integer, primary_key=True)
     title = Column(String(50), nullable=False)
-    attribute = Column(String(20), nullable=False)
     content = Column(String(1000), nullable=False)
     member_id = Column(Integer, ForeignKey('Member.id'), nullable=False)
-    board = Column(String(20), nullable=False)
+    board_id = Column(Integer, ForeignKey('Board.id'), nullable=False)
     created_at = Column(DateTime)
 
     def __repr__(self):
@@ -19,3 +18,4 @@ class PostEntity(Base):
         Post entity 필드값들을 리턴합니다.
         """
         return f"<User(id={self.id}, title='{self.title}', attribute='{self.attribute}', content='{self.content}', user_id='{self.user_id}', board='{self.board}', created_at='{self.created_at}')>"
+
