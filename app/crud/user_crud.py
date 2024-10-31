6743ea9647a7f625c714da518e198066fec04c8f
jeongseonwoong <jeongseonwoong@LAPTOP-JQUTIV0S>
2024-10-31 15:59:02 +0900

    feat/9

--- app/crud/user_crud.py
+++ app/crud/user_crud.py
@@ -1,3 +1,4 @@
+import bcrypt
 from sqlalchemy.orm import Session
 
 from app.models.user_entity import UserEntity
@@ -5,3 +6,7 @@ from app.models.user_entity import UserEntity
 
 def get_user_by_id(db: Session, id):
     return db.query(UserEntity).filter(UserEntity.id == id).first()
+
+def encrypt_password(user : UserEntity):
+    user.password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
+    return user
