418314ad6b74b428b6223c67e1d20251e2436893
jeongseonwoong <jeongseonwoong@LAPTOP-JQUTIV0S>
2024-10-31 15:44:29 +0900

    feat/11

--- app/crud/user_crud.py
+++ app/crud/user_crud.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import Session
 from app.models.user_entity import UserEntity
 
 
-def get_user_by_member_id(db: Session, member_id):
-    return db.query(UserEntity).filter(UserEntity.member_id == member_id).first()
+def get_user_by_id(db: Session, id):
+    return db.query(UserEntity).filter(UserEntity.id == id).first()
