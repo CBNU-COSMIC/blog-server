9058da0df2d7e6c505133c3d5e54a1f2bcab55bf
Hyunjiiing <107828202+Hyunjiiing@users.noreply.github.com>
2024-11-01 04:26:34 +0900

    생성 시 글의 속성은 일반만 가져야 한다 (#134)
    * refactor: 엔티티 파일 위치 변경
    
    * refactor: 엔티티 파일 위치 변경
    
    * feat: post 속성 제거 및 게시판 식별자 추가
    
    * refactor: 엔티티 추출 후 변경점
    
    * feat: 게시글 생성을 위한 로직 구현
    
    * test: 게시글 생성을 위한 테스트 코드 작성

---
+++ app/services/post_service.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from app.models.post import Post
+
+
+def create_post(title: str, content: str, member_id: int, board_id: int) -> Post:
+    """
+    게시글을 작성합니다.
+
+    @:param title 게시글의 제목
+    @:param content 게시글의 내용
+    @:param member_id 작성자의 식별자
+    @:param board_id 게시판의 식별자
+    @:return created_post 저장된 게시글
+    """
+    post = Post(id=None, title=title, content=content, member_id=member_id, board_id=board_id, created_at=datetime.now())
+    # created_post = 게시글 저장
+    # return created_post
