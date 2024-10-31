4beb9f4fad1b62966e251281ac1276f3097d33fa
jeongseonwoong <jeongseonwoong@LAPTOP-JQUTIV0S>
2024-10-31 15:01:53 +0900

    feat/122

--- app/models/user_orm.py
+++ app/models/user_orm.py
@@ -4,7 +4,7 @@ import datetime
 
 Base = declarative_base()
 
-class UserORM(Base):
+class UserDTO(Base):
     __tablename__ = 'Member'
 
     id = Column(String, primary_key=True, foreign_key=True)
@@ -18,14 +18,10 @@ class UserORM(Base):
     birth = Column(DateTime)
     email = Column(String)
 
-    def __init__(self, id, name, member_id, password, role, avatar, phone_number, student_number, birth, email):
-        self.id = id
-        self.name = name
-        self.member_id = member_id
-        self.password = password
-        self.role = role
-        self.avatar = avatar
-        self.phone_number = phone_number
-        self.student_number = student_number
-        self.birth = birth
-        self.email = email
+    def __repr__(self):
+        return f"<User(id={self.id}, name='{self.name}', member_id='{self.member_id}', password='{self.password}', role='{self.role}', avartar='{self.avatar}', phone_number='{self.phone_number}, student_number='{self.student_number}', birth='{self.birth}', email='{self.email}')>"
+
+    def __eq__(self, other):
+        if isinstance(other, UserDTO):
+            return self.id == other.id and self.name == other.name and self.member_id == other.member_id and self.password == other.password and self.role == other.role and self.avatar == other.avatar and self.phone_number == other.phone_number and self.student_number == other.student_number and self.birth == other.birth and self.email == other.email
+        return False
