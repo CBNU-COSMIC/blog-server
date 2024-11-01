2a24842a43e940d349c288448fde17cbbdbf5399
MinSeok Oh <minseok.dev@gmail.com>
2024-11-01 14:37:49 +0900

    fix: create_user 시 id 제거

--- app/crud/user_crud.py
+++ app/crud/user_crud.py
@@ -7,7 +7,9 @@ from app.models.user_entity import UserEntity
 
 
 def create_user(db: Session, user: User):
-    user_entity = UserEntity(id= user.id, name= user.name, member_id= user.member_id, password= user.password, role= user.role, avatar= user.avatar, phone_number= user.phone_number, student_number= user.student_number, birth= user.birth, email= user.email)
+    user_entity = UserEntity(name=user.name, member_id=user.member_id, password=user.password,
+                             role=user.role, avatar=user.avatar, phone_number=user.phone_number,
+                             student_number=user.student_number, birth=user.birth, email=user.email)
     return db.add(user_entity)
 
 
@@ -17,4 +19,3 @@ def get_user_by_id(db: Session, id: int) -> User:
     """
     user_entity = db.query(UserEntity).filter(UserEntity.id == id).first()
     return convert_to_user(user_entity) if user_entity else None
-
