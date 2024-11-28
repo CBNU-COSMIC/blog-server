634b1933032c55e895e721a9ea894f97735d2b98
MinSeok Oh <minseok.dev@gmail.com>
2024-11-28 21:18:13 +0900

    refactor: 배포를 위한 도커 스크립트 작성

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.router import router
 from app.util.auth_middleware import auth_middleware
@@ -8,5 +9,13 @@ app = FastAPI()
 
 app.include_router(router)
 app.middleware("http")(auth_middleware)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 init_db()
