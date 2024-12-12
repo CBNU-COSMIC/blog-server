42a11c46e9c7cf7c36eb162186fe3843e055e334
MinSeok Oh <minseok.dev@gmail.com>
2024-12-12 20:11:30 +0900

    feat: 게시글 삭제에 대한 인가 로직 구현
    Co-authored-by: Hyunjiiing <h.jilluminate@gmail.com>

--- app/routers/post_routers.py
+++ app/routers/post_routers.py
@@ -47,4 +47,4 @@ def update_post(request: Request, postId: int, post_update_dto: PostUpdateDTO, d
 @router.delete('/{postId}')
 def delete_post(request: Request, postId: int, db: Session = Depends(get_db)):
     user = request.state.user
-    return post_service.delete_post(user_id=user['username'], post_id=postId, db=db)
+    return post_service.delete_post(user_id=user['username'], role=user['role'], post_id=postId, db=db)
