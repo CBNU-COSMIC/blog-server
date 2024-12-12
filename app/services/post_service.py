c0fbf19a90b0ed992af90c24739eedefc76b3d0a
MinSeok Oh <minseok.dev@gmail.com>
2024-12-12 18:49:33 +0900

    fix: author가 없을 경우 기본 값 설정
    Co-authored-by: sinsohi <sohi8451@naver.com>

--- app/services/post_service.py
+++ app/services/post_service.py
@@ -51,11 +51,12 @@ def get_posts_by_board_id(board_id: str, page: int, db: Session) -> list:
     게시판의 게시글 목록을 조회합니다.
     """
     posts = post_crud.get_posts_by_board_id(db=db, board_id=board_id, page=page)
+
     return [
         PostsReadDTO(
             post_id=post.id,
             title=post.title,
-            author=post.member_id,
+            author=post.member_id if post.member_id else '',
             date=post.created_at,
             hits=post.hits,
             type='content' if post.board_id not in ['sw', 'cbnu', 'cse'] else 'link',
