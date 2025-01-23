217071f9dd8fb94e28dcd482701336b0a6b4a459
jeongseonwoong <tjsdnd1219@naver.com>
2025-01-23 17:25:18 +0900

    create PostJpaRepository interface to inherit JPA methods

--- core/src/main/java/org/cosmic/cafe/infra/PostJpaRepository.java
+++ core/src/main/java/org/cosmic/cafe/infra/PostJpaRepository.java
@@ -1,6 +1,6 @@
-package org.bee.metro.core.post.infra;
+package org.cosmic.cafe.infra;
 
-import org.bee.metro.core.post.entity.PostEntity;
+import org.cosmic.cafe.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostJpaRepository extends JpaRepository<PostEntity, Long> {
