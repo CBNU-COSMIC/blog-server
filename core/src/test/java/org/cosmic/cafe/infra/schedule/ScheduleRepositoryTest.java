8abd9f873672a3207ce64aa4f9983b096c76e278
minseok-oh <minseok.dev@gmail.com>
2025-01-28 21:24:08 +0900

    test: 일정 레포지토리 save 메서드 테스트 작성

---
+++ core/src/test/java/org/cosmic/cafe/infra/schedule/ScheduleRepositoryTest.java
@@ -0,0 +1,95 @@
+package org.cosmic.cafe.infra.schedule;
+
+import org.cosmic.cafe.context.RepositoryContext;
+import org.cosmic.cafe.domain.schedule.Color;
+import org.cosmic.cafe.domain.schedule.Schedule;
+import org.cosmic.cafe.domain.schedule.ScheduleRepository;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class ScheduleRepositoryTest extends RepositoryContext {
+
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+
+    @Nested
+    class save_메서드는 {
+
+        @Test
+        void 일정_객체를_저장한다() {
+            // given
+            String title = "title";
+            String content = "content";
+            UUID memberId = UUID.randomUUID();
+            LocalDateTime startDateTime = LocalDateTime.now();
+            LocalDateTime endDateTime = LocalDateTime.now().plusHours(1);
+            Color color = Color.RED;
+
+            // when
+            Schedule savedSchedule = scheduleRepository.save(Schedule.builder()
+                    .title(title)
+                    .content(content)
+                    .memberId(memberId)
+                    .startDateTime(startDateTime)
+                    .endDateTime(endDateTime)
+                    .color(color)
+                    .build());
+
+            // then
+            assertAll(
+                    () -> assertThat(savedSchedule.getId()).isNotNull(),
+                    () -> assertThat(savedSchedule.getTitle()).isEqualTo(title),
+                    () -> assertThat(savedSchedule.getContent()).isEqualTo(content),
+                    () -> assertThat(savedSchedule.getMemberId()).isEqualTo(memberId),
+                    () -> assertThat(savedSchedule.getStartDateTime()).isEqualTo(startDateTime),
+                    () -> assertThat(savedSchedule.getEndDateTime()).isEqualTo(endDateTime),
+                    () -> assertThat(savedSchedule.getColor()).isEqualTo(color)
+            );
+        }
+
+        @Test
+        void 존재하는_일정_객체는_수정한다() {
+            // given
+            String title = "title";
+            String updatedTitle = "updated title";
+            String content = "content";
+            UUID memberId = UUID.randomUUID();
+            LocalDateTime startDateTime = LocalDateTime.now();
+            LocalDateTime endDateTime = LocalDateTime.now().plusHours(1);
+            Color color = Color.RED;
+
+            Schedule savedSchedule = scheduleRepository.save(Schedule.builder()
+                    .title(title)
+                    .content(content)
+                    .memberId(memberId)
+                    .startDateTime(startDateTime)
+                    .endDateTime(endDateTime)
+                    .color(color)
+                    .build());
+
+
+            // when
+            Schedule updatedSchedule = scheduleRepository.save(Schedule.builder()
+                    .id(savedSchedule.getId())
+                    .title(updatedTitle)
+                    .content(content)
+                    .memberId(memberId)
+                    .startDateTime(startDateTime)
+                    .endDateTime(endDateTime)
+                    .color(color)
+                    .build());
+
+
+            // then
+            assertThat(savedSchedule.getId()).isEqualTo(updatedSchedule.getId());
+            assertThat(updatedSchedule.getTitle()).isEqualTo(updatedTitle);
+        }
+    }
+}
