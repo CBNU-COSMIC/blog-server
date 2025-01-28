c92d7961614369eaba1e9d4f94efd98c29f16ae0
minseok-oh <minseok.dev@gmail.com>
2025-01-28 21:25:50 +0900

    fix: 일정 식별자 타입 Long -> UUID 변경

--- core/src/main/java/org/cosmic/cafe/infra/schedule/ScheduleJpaRepository.java
+++ core/src/main/java/org/cosmic/cafe/infra/schedule/ScheduleJpaRepository.java
@@ -3,5 +3,7 @@ package org.cosmic.cafe.infra.schedule;
 import org.cosmic.cafe.infra.schedule.entity.ScheduleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ScheduleJpaRepository extends JpaRepository<ScheduleEntity, Long> {
+import java.util.UUID;
+
+public interface ScheduleJpaRepository extends JpaRepository<ScheduleEntity, UUID> {
 }
