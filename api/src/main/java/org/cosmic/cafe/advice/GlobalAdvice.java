a2f3bd3e44ed27ce473c0782b9cfa3c693835ed7
MinSeok Oh <minseok.dev@gmail.com>
2025-02-02 22:09:04 +0900

    API를 위한 기본적인 세팅을 구현합니다 (#196)
    * setting: spring boot starter web 의존성 추가
    
    * feat: 전역적인 예외 처리 Advice 구현
    
    * feat: CORS 해결을 위한 WebConfig 구현
    
    * setting: 허가된 클라이언트 Origin 작성
    
    Close #195

---
+++ api/src/main/java/org/cosmic/cafe/advice/GlobalAdvice.java
@@ -0,0 +1,38 @@
+package org.cosmic.cafe.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.cosmic.cafe.exception.ErrorResponse;
+import org.cosmic.cafe.exception.GlobalErrorCode;
+import org.cosmic.cafe.exception.type.BusinessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalAdvice {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException exception) {
+        ErrorResponse errorResponse = ErrorResponse.of(exception.getErrorCode().getCode(), exception.getErrorCode().getMessage());
+
+        log.error("{}", exception);
+
+        return ResponseEntity
+                .status(exception.getStatusCode())
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleInternalServerError(Exception exception) {
+        ErrorResponse errorResponse = ErrorResponse.of(GlobalErrorCode.INTERNAL_SERVER_ERROR.getCode(),
+                GlobalErrorCode.INTERNAL_SERVER_ERROR.getMessage());
+
+        log.error("{}", exception);
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(errorResponse);
+    }
+}
