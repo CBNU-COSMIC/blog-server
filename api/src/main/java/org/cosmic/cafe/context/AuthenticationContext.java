3acbf7d4e6e06b0e17ee484c3e3244f497fd05f3
minseok-oh <minseok.dev@gmail.com>
2025-02-02 22:32:09 +0900

    feat: 로그인 정보를 위한 Argument Resolver 구현

--- api/src/main/java/org/cosmic/cafe/context/AuthenticationContext.java
+++ api/src/main/java/org/cosmic/cafe/context/AuthenticationContext.java
@@ -1,9 +1,17 @@
 package org.cosmic.cafe.context;
 
 import org.cosmic.cafe.dto.LoginPayload;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
+@Component
+@RequestScope
 public class AuthenticationContext {
     private LoginPayload principal;
+
+    public LoginPayload getPrincipal() {
+        return principal;
+    }
 
     public void setPrincipal(LoginPayload principal) {
         this.principal = principal;
