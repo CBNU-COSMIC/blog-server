f49b94962549dea74577a3afa5297454d77f0ed1
minseok-oh <minseok.dev@gmail.com>
2025-02-02 22:33:03 +0900

    setting: 로그인 정보 Resolver 추가

--- api/src/main/java/org/cosmic/cafe/config/WebConfig.java
+++ api/src/main/java/org/cosmic/cafe/config/WebConfig.java
@@ -1,12 +1,20 @@
 package org.cosmic.cafe.config;
 
+import lombok.RequiredArgsConstructor;
+import org.cosmic.cafe.controller.annotation.LoginArgumentResolver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginArgumentResolver loginArgumentResolver;
 
     @Value("${client.allowed.origins}")
     private String allowedOrigins;
@@ -18,5 +26,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginArgumentResolver);
     }
 }
