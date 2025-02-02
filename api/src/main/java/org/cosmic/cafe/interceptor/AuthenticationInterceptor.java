b9f5d9a18076cffeefaf1358c2881907b4c22d3e
minseok-oh <minseok.dev@gmail.com>
2025-02-02 22:51:27 +0900

    feat: 필요한 Interceptor 생성
    - 인증, 인가를 위해 생성합니다

---
+++ api/src/main/java/org/cosmic/cafe/interceptor/AuthenticationInterceptor.java
@@ -0,0 +1,16 @@
+package org.cosmic.cafe.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+}
