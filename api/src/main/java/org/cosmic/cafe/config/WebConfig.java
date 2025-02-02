7a89d762b70cf5d1e3ad60cc3e55e2792927881e
minseok-oh <minseok.dev@gmail.com>
2025-02-02 22:51:52 +0900

    setting: WebConfig 내에 Interceptor 설정

--- api/src/main/java/org/cosmic/cafe/config/WebConfig.java
+++ api/src/main/java/org/cosmic/cafe/config/WebConfig.java
@@ -2,10 +2,12 @@ package org.cosmic.cafe.config;
 
 import lombok.RequiredArgsConstructor;
 import org.cosmic.cafe.controller.annotation.LoginArgumentResolver;
+import org.cosmic.cafe.interceptor.AuthenticationInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -14,6 +16,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
+    private final AuthenticationInterceptor authenticationInterceptor;
+    private final AuthenticationInterceptor authorizationInterceptor;
     private final LoginArgumentResolver loginArgumentResolver;
 
     @Value("${client.allowed.origins}")
@@ -26,6 +30,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .addPathPatterns("/**");
+
+        registry.addInterceptor(authorizationInterceptor)
+                .addPathPatterns("/**");
     }
 
     @Override
