94a750d8ab02b46bf0a5883f6358c91469c39d45
minseok-oh <minseok.dev@gmail.com>
2025-02-03 09:13:51 +0900

    feat: 인가 Interceptor 구현
    - Admin 어노테이션을 통해 확인합니다

--- api/src/main/java/org/cosmic/cafe/interceptor/AuthorizationInterceptor.java
+++ api/src/main/java/org/cosmic/cafe/interceptor/AuthorizationInterceptor.java
@@ -3,14 +3,39 @@ package org.cosmic.cafe.interceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.cosmic.cafe.context.AuthenticationContext;
+import org.cosmic.cafe.controller.annotation.Admin;
+import org.cosmic.cafe.domain.member.Role;
+import org.cosmic.cafe.exception.ApiErrorCode;
+import org.cosmic.cafe.exception.type.AuthorizationException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 @RequiredArgsConstructor
 public class AuthorizationInterceptor implements HandlerInterceptor {
+
+    private final AuthenticationContext authenticationContext;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (shouldNotHandle((HandlerMethod) handler)) {
+            return true;
+        }
+
+        Role role = authenticationContext.getPrincipal().role();
+        if (role != Role.PRESIDENT) {
+            throw new AuthorizationException("권한이 없습니다.", ApiErrorCode.FORBIDDEN);
+        }
         return true;
+    }
+
+    private boolean shouldNotHandle(HandlerMethod handler) {
+        HandlerMethod handlerMethod = handler;
+        if (!handlerMethod.hasMethodAnnotation(Admin.class)) {
+            return true;
+        }
+        return false;
     }
 }
