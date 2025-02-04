b4f62f0c2e1abfe58ae96b2fe6019148b4952d37
Sin Sohi <sohi8451@naver.com>
2025-02-04 16:17:04 +0900

    feat: 댓글 레포지토리 인터페이스 생성

---
+++ core/src/main/java/org/cosmic/cafe/domain/Comment/CommentRepository.java
@@ -0,0 +1,14 @@
+package org.cosmic.cafe.domain.Comment;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CommentRepository {
+    Comment save(Comment comment);
+    Comment findById(UUID id);
+    List<Comment> findByPostId(UUID postId);
+    List<Comment> findByParentId(UUID parentId);
+    void delete(UUID id);
+    void deleteByPostId(UUID postId);
+    void deleteByParentId(UUID parentId);
+}
