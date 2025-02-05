ac43e8037e63fa5e1c549bdeea9a9508ed1b238b
Sin Sohi <sohi8451@naver.com>
2025-02-06 02:04:57 +0900

    test: 댓글 레포지토리 테스트를 위한 환경 구성

--- core/src/test/java/org/cosmic/cafe/context/RepositoryContext.java
+++ core/src/test/java/org/cosmic/cafe/context/RepositoryContext.java
@@ -1,5 +1,7 @@
 package org.cosmic.cafe.context;
 
+import org.cosmic.cafe.infra.comment.CommentCoreRepository;
+import org.cosmic.cafe.infra.comment.CommentJpaRepository;
 import org.cosmic.cafe.infra.schedule.ScheduleCoreRepository;
 import org.cosmic.cafe.infra.schedule.ScheduleJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +9,12 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 @DataJpaTest
-@Import(ScheduleCoreRepository.class)
+@Import({ScheduleCoreRepository.class, CommentCoreRepository.class})
 public abstract class RepositoryContext {
 
     @Autowired
     protected ScheduleJpaRepository scheduleJpaRepository;
+
+    @Autowired
+    protected CommentJpaRepository commentJpaRepository;
 }
