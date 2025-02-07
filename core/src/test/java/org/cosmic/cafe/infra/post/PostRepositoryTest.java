00cc40db874bd161a6946d06c23b9c053791761c
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-07 12:20:09 +0900

    test: 게시글 레포지토리 save 메서드 테스트 작성

--- core/src/test/java/org/cosmic/cafe/infra/post/PostRepositoryTest.java
+++ core/src/test/java/org/cosmic/cafe/infra/post/PostRepositoryTest.java
@@ -1,2 +1,51 @@
-package org.cosmic.cafe.infra.post;public class PostRepository {
+package org.cosmic.cafe.infra.post;
+
+import org.cosmic.cafe.context.RepositoryContext;
+import org.cosmic.cafe.domain.post.Post;
+import org.cosmic.cafe.domain.post.PostRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PostRepositoryTest extends RepositoryContext {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Nested
+    class save_메서드는{
+
+        @Test
+        void 게시글을_저장한다(){
+
+            //given
+            String boardId = "게시판";
+            String title = "TEST TITLE";
+            String content = "TEST CONTENT";
+            Long hits = 1L;
+
+            //when
+            Post savedPost = postRepository.save(Post.builder()
+                    .boardId(boardId)
+                    .title(title)
+                    .content(content)
+                    .hits(hits)
+                    .build());
+
+            //then
+            Assertions.assertAll(
+                    () -> assertThat(savedPost.getId()).isNotNull(),
+                    () -> assertThat(savedPost.getBoardId()).isEqualTo(boardId),
+                    () -> assertThat(savedPost.getTitle()).isEqualTo(title),
+                    () -> assertThat(savedPost.getContent()).isEqualTo(content),
+                    () -> assertThat(savedPost.getHits()).isEqualTo(hits)
+                    );
+        }
+    }
+
 }
