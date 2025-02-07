e453be5bd08285964d57b9cd7b5e05c109df9aaf
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-07 12:20:42 +0900

    test: 게시글 레포지토리 findById 메서드 테스트 작성

--- core/src/test/java/org/cosmic/cafe/infra/post/PostRepositoryTest.java
+++ core/src/test/java/org/cosmic/cafe/infra/post/PostRepositoryTest.java
@@ -48,4 +48,34 @@ public class PostRepositoryTest extends RepositoryContext {
         }
     }
 
+    @Nested
+    class findById_메서드는{
+
+        @Test
+        void 해당_아이디가_존재하면_게시글_객체를_반환한다(){
+
+            //given
+            String boardId = "게시판";
+            String title = "TEST TITLE";
+            String content = "TEST CONTENT";
+            Long hits = 1L;
+
+            Post savedPost = postRepository.save(Post.builder()
+                    .boardId(boardId)
+                    .title(title)
+                    .content(content)
+                    .hits(hits)
+                    .build());
+
+            //when
+            Post foundPost = postRepository.findById(savedPost.getId()).orElse(null);
+
+            //then
+            Assertions.assertAll(
+                    ()-> assertThat(foundPost).isNotNull(),
+                    ()-> assertThat(foundPost.getId()).isEqualTo(savedPost.getId())
+            );
+        }
+    }
+
 }
