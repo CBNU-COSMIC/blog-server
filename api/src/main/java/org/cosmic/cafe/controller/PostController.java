cf7e58eecbb3a5854378f631e163bf2816b66266
Sin Sohi <sohi8451@naver.com>
2025-02-15 19:49:43 +0900

    feat: 게시글 컨트롤러 기본 틀 작성

---
+++ api/src/main/java/org/cosmic/cafe/controller/PostController.java
@@ -0,0 +1,15 @@
+package org.cosmic.cafe.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.cosmic.cafe.application.post.PostService;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+}
