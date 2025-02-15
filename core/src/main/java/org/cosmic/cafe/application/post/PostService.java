328249db1a5afdb7c75454c8498b2881e6cab3cc
Sin Sohi <sohi8451@naver.com>
2025-02-15 19:21:21 +0900

    feat: 식별자를 통한 게시글 조회 로직 구현
    - 존재하지 않는 게시글에 대한 에러 코드 생성

--- core/src/main/java/org/cosmic/cafe/application/post/PostService.java
+++ core/src/main/java/org/cosmic/cafe/application/post/PostService.java
@@ -1,8 +1,13 @@
 package org.cosmic.cafe.application.post;
 
 import lombok.RequiredArgsConstructor;
+import org.cosmic.cafe.domain.post.Post;
 import org.cosmic.cafe.domain.post.PostRepository;
+import org.cosmic.cafe.domain.post.exception.PostErrorCode;
+import org.cosmic.cafe.exception.type.BadRequestException;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -10,5 +15,14 @@ public class PostService {
 
     private final PostRepository postRepository;
 
+    public void update(String title, String content, UUID memberId, UUID postId) {
+        Post post = getPostById(postId);
+
+    }
+
+    private Post getPostById(UUID postId) {
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new BadRequestException("게시글이 존재하지 않습니다.", PostErrorCode.NOT_FOUND));
+    }
 
 }
--- core/src/main/java/org/cosmic/cafe/domain/post/exception/PostErrorCode.java
+++ core/src/main/java/org/cosmic/cafe/domain/post/exception/PostErrorCode.java
@@ -4,7 +4,8 @@ import org.cosmic.cafe.exception.ErrorCode;
 
 public enum PostErrorCode implements ErrorCode {
 
-    ARGUMENT_IS_NULL("M001", "인자는 null일 수 없습니다.");
+    ARGUMENT_IS_NULL("P001", "인자는 null일 수 없습니다."),
+    NOT_FOUND("POO2", "게시글이 존재하지 않습니다.");
 
     private final String code;
     private final String message;
