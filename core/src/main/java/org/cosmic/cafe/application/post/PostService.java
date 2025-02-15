e078f44ce3480d7abb27a131ad53d574a4f47dc4
Sin Sohi <sohi8451@naver.com>
2025-02-15 19:35:17 +0900

    feat: 게시글 수정 권한 제한 로직 구현
    - 게시글 수정 권한 에러 코드 작성

--- core/src/main/java/org/cosmic/cafe/application/post/PostService.java
+++ core/src/main/java/org/cosmic/cafe/application/post/PostService.java
@@ -5,6 +5,7 @@ import org.cosmic.cafe.domain.post.Post;
 import org.cosmic.cafe.domain.post.PostRepository;
 import org.cosmic.cafe.domain.post.exception.PostErrorCode;
 import org.cosmic.cafe.exception.type.BadRequestException;
+import org.cosmic.cafe.exception.type.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -18,11 +19,15 @@ public class PostService {
     public void update(String title, String content, UUID memberId, UUID postId) {
         Post post = getPostById(postId);
 
+        if (!(post.getMemberId() == memberId)) {
+            throw new BadRequestException("해당 게시글 수정 권한이 없습니다. 게시글 아이디 : %s".formatted(postId), PostErrorCode.UPDATE_PERMISSION_DENIED);
+        }
+
     }
 
     private Post getPostById(UUID postId) {
         return postRepository.findById(postId)
-                .orElseThrow(() -> new BadRequestException("게시글이 존재하지 않습니다.", PostErrorCode.NOT_FOUND));
+                .orElseThrow(() -> new NotFoundException("게시글이 존재하지 않습니다.", PostErrorCode.NOT_FOUND));
     }
 
 }
