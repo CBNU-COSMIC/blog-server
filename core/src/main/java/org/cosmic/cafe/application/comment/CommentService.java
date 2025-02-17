e21c31e517cfa9289a1d8415eccbc50c1d96bce7
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-17 12:18:16 +0900

    feat: 댓글 서비스계층 트랜잭션을 default로  읽기 전용으로설정

--- core/src/main/java/org/cosmic/cafe/application/comment/CommentService.java
+++ core/src/main/java/org/cosmic/cafe/application/comment/CommentService.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CommentService {
 
     private final CommentRepository commentRepository;
-    
+
 }
