4d48eddd2ee5d48c0974b8fcad510e96e4cf03a0
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-17 12:59:18 +0900

    feat: 댓글 수정을 위한 DTO 생성

---
+++ core/src/main/java/org/cosmic/cafe/application/comment/dto/ModifyCommentDTO.java
@@ -0,0 +1,14 @@
+package org.cosmic.cafe.application.comment.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class ModifyCommentDTO {
+    private String content;
+    private UUID postId;
+    private UUID parentId;
+}
