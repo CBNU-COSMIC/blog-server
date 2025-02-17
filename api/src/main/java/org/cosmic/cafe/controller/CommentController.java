d3f5200a3dafd7a91b03674bd4196f8673bd14f5
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-17 13:01:35 +0900

    feat: 댓글 수정을 위한 api 구현

--- api/src/main/java/org/cosmic/cafe/controller/CommentController.java
+++ api/src/main/java/org/cosmic/cafe/controller/CommentController.java
@@ -2,8 +2,10 @@ package org.cosmic.cafe.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.cosmic.cafe.application.comment.CommentService;
+import org.cosmic.cafe.application.comment.dto.ModifyCommentDTO;
 import org.cosmic.cafe.application.comment.dto.SaveCommentDTO;
 import org.cosmic.cafe.controller.annotation.Login;
+import org.cosmic.cafe.domain.Comment.Comment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,5 +28,12 @@ public class CommentController {
     public ResponseEntity<Void> deleteComment(@PathVariable("commentId")String commentId, @Login UUID memberId){
         commentService.deleteComment(commentId,memberId);
         return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("{commentId}")
+    public ResponseEntity<Comment> modifyComment(@RequestBody ModifyCommentDTO modifyCommentDTO, @PathVariable("commentId")String commentId, @Login UUID memberId){
+        Comment comment = commentService.modifyComment(modifyCommentDTO.getContent(),modifyCommentDTO.getPostId(),
+                modifyCommentDTO.getParentId(),commentId,memberId);
+        return ResponseEntity.ok(comment);
     }
 }
