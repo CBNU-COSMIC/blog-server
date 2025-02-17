7e05f6b184617cc430de00e73a03737fa98a13d6
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-17 16:11:39 +0900

    test: 게시글 ID로 댓글 조회 테스트

--- core/src/test/java/org/cosmic/cafe/application/schedule/CommentServiceTest.java
+++ core/src/test/java/org/cosmic/cafe/application/schedule/CommentServiceTest.java
@@ -4,9 +4,11 @@ package org.cosmic.cafe.application.schedule;
 import org.assertj.core.api.Assertions;
 import org.cosmic.cafe.application.comment.dto.SaveCommentDTO;
 import org.cosmic.cafe.context.ServiceContext;
+import org.cosmic.cafe.domain.Comment.Comment;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.*;
@@ -30,5 +32,25 @@ public class CommentServiceTest extends ServiceContext {
             assertThat(commentRepository.findById(commentId).isPresent()).isTrue();
         }
     }
-    
+
+    @Nested
+    class 댓글_조회_테스트{
+
+        @Test
+        void 게시글_아이디로_댓글을_조회할_수_있다(){
+            //given
+            UUID postID = UUID.randomUUID();
+            Comment comment = Comment.builder()
+                            .content("content1").postId(postID).build();
+
+            commentRepository.save(comment);
+
+            //when
+            List<Comment> comments = commentRepository.findByPostId(postID);
+
+            //then
+            assertThat(comments).extracting(Comment::getContent).containsExactlyInAnyOrder("content1");
+        }
+
+    }
 }
