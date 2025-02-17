52856df12675060154a8eaa5df701a5496675a2b
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-17 16:04:54 +0900

    test: 댓글 저장 테스트

--- core/src/test/java/org/cosmic/cafe/application/schedule/CommentServiceTest.java
+++ core/src/test/java/org/cosmic/cafe/application/schedule/CommentServiceTest.java
@@ -1,7 +1,34 @@
 package org.cosmic.cafe.application.schedule;
 
 
+import org.assertj.core.api.Assertions;
+import org.cosmic.cafe.application.comment.dto.SaveCommentDTO;
 import org.cosmic.cafe.context.ServiceContext;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class CommentServiceTest extends ServiceContext {
+
+    @Nested
+    class 댓글_저장_테스트{
+
+        @Test
+        void 댓글을_정상적으로_저장할_수_있다(){
+            //given
+            UUID memberId = UUID.randomUUID();
+            UUID parentId = UUID.randomUUID();
+            UUID postID = UUID.randomUUID();
+
+            //when
+            UUID commentId = commentService.saveComment(memberId, "content", postID, parentId);
+
+            //then
+            assertThat(commentRepository.findById(commentId).isPresent()).isTrue();
+        }
+    }
+    
 }
