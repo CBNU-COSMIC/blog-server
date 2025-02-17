796cd8ca07365b7c5cc5564680e3a75a0eb703c7
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-17 12:52:05 +0900

    feat: 댓글 삭제를 위한api 구현

--- api/src/main/java/org/cosmic/cafe/controller/CommentController.java
+++ api/src/main/java/org/cosmic/cafe/controller/CommentController.java
@@ -5,10 +5,7 @@ import org.cosmic.cafe.application.comment.CommentService;
 import org.cosmic.cafe.application.comment.dto.SaveCommentDTO;
 import org.cosmic.cafe.controller.annotation.Login;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -23,5 +20,11 @@ public class CommentController {
     public ResponseEntity<UUID> saveComment(@RequestBody SaveCommentDTO commentDTO, @Login UUID memberId) {
         UUID commentId = commentService.saveComment(memberId, commentDTO.getContent(), commentDTO.getPostId(), commentDTO.getParentId());
         return ResponseEntity.ok(commentId);
+    }
+
+    @DeleteMapping("{commentId}")
+    public ResponseEntity<Void> deleteComment(@PathVariable("commentId")String commentId, @Login UUID memberId){
+        commentService.deleteComment(commentId,memberId);
+        return ResponseEntity.ok().build();
     }
 }
