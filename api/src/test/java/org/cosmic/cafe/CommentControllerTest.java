71da0367205305ae5c8801f49f276d882999bc0c
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-17 17:43:08 +0900

    feat:댓글 컨트롤러 테스트 기본 설정 추가

---
+++ api/src/test/java/org/cosmic/cafe/CommentControllerTest.java
@@ -0,0 +1,6 @@
+package org.cosmic.cafe;
+
+import org.cosmic.cafe.context.ControllerTest;
+
+public class CommentControllerTest extends ControllerTest {
+}
--- api/src/test/java/org/cosmic/cafe/context/ControllerTest.java
+++ api/src/test/java/org/cosmic/cafe/context/ControllerTest.java
@@ -1,15 +1,18 @@
 package org.cosmic.cafe.context;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.cosmic.cafe.CommentControllerTest;
 import org.cosmic.cafe.application.AuthService;
+import org.cosmic.cafe.application.comment.CommentService;
 import org.cosmic.cafe.controller.AuthController;
+import org.cosmic.cafe.controller.CommentController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest({AuthController.class})
+@WebMvcTest({AuthController.class, CommentController.class})
 @AutoConfigureMockMvc
 public abstract class ControllerTest {
 
@@ -24,4 +27,7 @@ public abstract class ControllerTest {
 
     @Autowired
     protected ObjectMapper objectMapper;
+
+    @MockitoBean
+    protected CommentService commentService;
 }
