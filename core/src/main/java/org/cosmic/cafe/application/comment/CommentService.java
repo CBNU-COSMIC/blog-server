4cd9e3cfefe332734a5685f669ac6b88c2e26627
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-17 14:29:13 +0900

    feat: 댓글 수정을 위한 서비스 계층 메서드 추가

--- core/src/main/java/org/cosmic/cafe/application/comment/CommentService.java
+++ core/src/main/java/org/cosmic/cafe/application/comment/CommentService.java
@@ -1,8 +1,11 @@
 package org.cosmic.cafe.application.comment;
 
 import lombok.RequiredArgsConstructor;
+import org.cosmic.cafe.application.comment.dto.CommentResponseDTO;
 import org.cosmic.cafe.domain.Comment.Comment;
 import org.cosmic.cafe.domain.Comment.CommentRepository;
+import org.cosmic.cafe.domain.Comment.exception.CommentErrorCode;
+import org.cosmic.cafe.exception.type.BadRequestException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +23,28 @@ public class CommentService {
     @Transactional
     public UUID saveComment(UUID userId, String content, UUID postId, UUID parentId) {
 
+        Comment comment = getComment(userId, content, postId, parentId);
+
+        return commentRepository.save(comment).getId();
+    }
+
+    @Transactional
+    public void deleteComment(String stringCommentId, UUID memberId) {
+        UUID commentId = UUID.fromString(stringCommentId);
+        Optional<Comment> comment = commentRepository.findById(commentId);
+        comment.ifPresent(comment1 -> comment1.validateOwner(memberId));
+        commentRepository.deleteById(memberId);
+    }
+
+    @Transactional
+    public CommentResponseDTO modifyComment(String content, String commentId, UUID memberId) {
+        Comment comment = commentRepository.findById(UUID.fromString(commentId))
+                .orElseThrow(() -> new BadRequestException("해당 댓글이 존재하지 않습니다.", CommentErrorCode.NO_SUCH_COMMENT));
+        comment.modifyContent(memberId, content);
+        return new CommentResponseDTO(comment);
+    }
+
+    private static Comment getComment(UUID userId, String content, UUID postId, UUID parentId) {
         Comment comment = Comment.builder()
                 .content(content)
                 .postId(postId)
@@ -27,14 +52,6 @@ public class CommentService {
                 .userId(userId)
                 .createdAt(LocalDateTime.now())
                 .build();
-
-        return commentRepository.save(comment).getId();
-    }
-
-    public void deleteComment(String stringCommentId, UUID memberId) {
-        UUID commentId = UUID.fromString(stringCommentId);
-        Optional<Comment> comment = commentRepository.findById(commentId);
-        comment.ifPresent(comment1 -> comment1.validateOwner(memberId));
-        commentRepository.deleteById(memberId);
+        return comment;
     }
 }
