9988c93ea8520815409315ba2790b9098ded5567
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-17 12:25:07 +0900

    feat: 댓글 저장을 위한 api 구현

---
+++ api/src/main/java/org/cosmic/cafe/controller/CommentController.java
@@ -0,0 +1,27 @@
+package org.cosmic.cafe.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.cosmic.cafe.application.comment.CommentService;
+import org.cosmic.cafe.application.comment.dto.SaveCommentDTO;
+import org.cosmic.cafe.controller.annotation.Login;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RequestMapping("/api/comment")
+@RequiredArgsConstructor
+@RestController
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("")
+    public ResponseEntity<Long> saveComment(@RequestBody SaveCommentDTO commentDTO, @Login UUID memberId) {
+        Long commentId = commentService.saveComment(memberId, commentDTO.getContent(), commentDTO.getPostId(), commentDTO.getParentId());
+        return ResponseEntity.ok(commentId);
+    }
+}
--- core/src/main/java/org/cosmic/cafe/application/comment/CommentService.java
+++ core/src/main/java/org/cosmic/cafe/application/comment/CommentService.java
@@ -1,9 +1,12 @@
 package org.cosmic.cafe.application.comment;
 
 import lombok.RequiredArgsConstructor;
+import org.cosmic.cafe.domain.Comment.Comment;
 import org.cosmic.cafe.domain.Comment.CommentRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @Transactional(readOnly = true)
@@ -12,4 +15,8 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
 
+    @Transactional
+    public Long saveComment(UUID memberId, String content, UUID postId, UUID parentId) {
+        Comment comment = Comment
+    }
 }
