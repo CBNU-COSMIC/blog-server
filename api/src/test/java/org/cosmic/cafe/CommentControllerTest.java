d324f234f3ebfb5ded1ab48fc69828112b46f9ce
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-17 18:45:38 +0900

    test: 댓글 작성 테스트 코드 작성

--- api/src/test/java/org/cosmic/cafe/CommentControllerTest.java
+++ api/src/test/java/org/cosmic/cafe/CommentControllerTest.java
@@ -1,6 +1,44 @@
 package org.cosmic.cafe;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.cosmic.cafe.application.comment.dto.SaveCommentDTO;
 import org.cosmic.cafe.context.ControllerTest;
+import org.cosmic.cafe.dto.LoginPayload;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import static org.mockito.BDDMockito.given;
+import org.springframework.mock.web.MockHttpSession;
+import org.cosmic.cafe.domain.member.Role;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+
+import java.util.UUID;
 
 public class CommentControllerTest extends ControllerTest {
+
+    @Nested
+    class 댓글_작성{
+
+        @Test
+        void 올바른_댓글은_200을_반환한다() throws Exception {
+            SaveCommentDTO saveCommentDTO = new SaveCommentDTO();
+            saveCommentDTO.setPostId(UUID.randomUUID());
+            saveCommentDTO.setContent("content");
+            saveCommentDTO.setParentId(UUID.randomUUID());
+            UUID memberId = UUID.randomUUID();
+
+            MockHttpSession session = new MockHttpSession();
+            session.setAttribute("loginPayload", new LoginPayload(memberId, Role.GUEST));
+
+            mockMvc.perform(post("/api/comment")
+                    .session(session).contentType("application/json")
+                    .content(objectMapper.writeValueAsString(saveCommentDTO)))
+                    .andExpect(status().isOk());
+        }
+    }
+    
+
+
 }
