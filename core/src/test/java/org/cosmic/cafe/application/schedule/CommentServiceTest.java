f32094401abbc08c151f98c6666ba5f388f8fd0f
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-17 16:44:08 +0900

    test: 댓글 삭제 테스트

--- core/src/test/java/org/cosmic/cafe/application/schedule/CommentServiceTest.java
+++ core/src/test/java/org/cosmic/cafe/application/schedule/CommentServiceTest.java
@@ -1,25 +1,25 @@
 package org.cosmic.cafe.application.schedule;
 
 
-import org.assertj.core.api.Assertions;
-import org.cosmic.cafe.application.comment.dto.SaveCommentDTO;
 import org.cosmic.cafe.context.ServiceContext;
 import org.cosmic.cafe.domain.Comment.Comment;
+import org.cosmic.cafe.exception.type.BadRequestException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CommentServiceTest extends ServiceContext {
 
     @Nested
-    class 댓글_저장_테스트{
+    class 댓글_저장_테스트 {
 
         @Test
-        void 댓글을_정상적으로_저장할_수_있다(){
+        void 댓글을_정상적으로_저장할_수_있다() {
             //given
             UUID memberId = UUID.randomUUID();
             UUID parentId = UUID.randomUUID();
@@ -34,14 +34,14 @@ public class CommentServiceTest extends ServiceContext {
     }
 
     @Nested
-    class 댓글_조회_테스트{
+    class 댓글_조회_테스트 {
 
         @Test
-        void 게시글_아이디로_댓글을_조회할_수_있다(){
+        void 게시글_아이디로_댓글을_조회할_수_있다() {
             //given
             UUID postID = UUID.randomUUID();
             Comment comment = Comment.builder()
-                            .content("content1").postId(postID).build();
+                    .content("content1").postId(postID).build();
 
             commentRepository.save(comment);
 
@@ -52,5 +52,36 @@ public class CommentServiceTest extends ServiceContext {
             assertThat(comments).extracting(Comment::getContent).containsExactlyInAnyOrder("content1");
         }
 
+    }
+
+    @Nested
+    class 댓글_삭제_테스트 {
+
+        @Test
+        void 본인_댓글을_삭제할_수_있다(){
+            //given
+            UUID memberId = UUID.randomUUID();
+            Comment savedComment = commentRepository.save(Comment.builder().userId(memberId).content("content").build());
+            String commentId = savedComment.getId().toString();
+
+            //when
+            commentService.deleteComment(commentId,memberId);
+
+            //then
+            Comment foundComment = commentRepository.findById(savedComment.getId()).orElse(null);
+            assertThat(foundComment).isNull();
+        }
+
+        @Test
+        void 본인이_아닐_경우_댓글_삭제가_불가능하다(){
+
+            //given
+            UUID memberId = UUID.randomUUID();
+            Comment savedComment = commentRepository.save(Comment.builder().userId(memberId).content("content").build());
+            String commentId = savedComment.getId().toString();
+
+            //expect
+            Assertions.assertThrows(BadRequestException.class,()->commentService.deleteComment(commentId,UUID.randomUUID()));
+        }
     }
 }
