83182258abd181898813ebe30905800031382b36
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-17 16:01:54 +0900

    refactor: CommentRepository DI 코드 추가

--- core/src/test/java/org/cosmic/cafe/context/ServiceContext.java
+++ core/src/test/java/org/cosmic/cafe/context/ServiceContext.java
@@ -2,6 +2,7 @@ package org.cosmic.cafe.context;
 
 import org.cosmic.cafe.application.comment.CommentService;
 import org.cosmic.cafe.application.schedule.ScheduleService;
+import org.cosmic.cafe.domain.Comment.CommentRepository;
 import org.cosmic.cafe.domain.schedule.ScheduleRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,9 @@ public abstract class ServiceContext {
 
     @Autowired
     protected CommentService commentService;
+
+    @Autowired
+    protected CommentRepository commentRepository;
 
     @AfterEach
     void clear() {
