5fb4397baaa07c7b9a9b021da079eb7e4ed063f5
Sin Sohi <sohi8451@naver.com>
2025-02-23 22:31:19 +0900

    feat: 공지사항 크롤링 서비스 구현체 생성

---
+++ crawl/src/main/java/org/cosmic/cafe/NoticeCrawlingService.java
@@ -0,0 +1,11 @@
+package org.cosmic.cafe;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoticeCrawlingService implements CrawlingService {
+    @Override
+    public void crawlPosts() {
+
+    }
+}
